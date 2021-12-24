4c449a600e045a98249a7b6f0cc4546130b7e910
Rahul Singh <rahulkumar.singh@milwaukeetool.com>
2021-12-24 05:32:50 -0600

    added appium dependency and created folder structure

---
+++ src/main/java/app/FrameworkApp.java
@@ -0,0 +1,14 @@
+package app;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class FrameworkApp {
+
+
+    public DesiredCapabilities init(){
+        return new DesiredCapabilities();
+    }
+
+    DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+
+}
