75b7636d8bfd8579c7639f8bd4c520ab73deb103
Rahul Singh <rahulkumar.singh@milwaukeetool.com>
2021-12-24 06:50:27 -0600

    added capability mapper class to map the capability string with the global constant

--- src/main/java/core/CapabilityMapper.java
+++ src/main/java/core/CapabilityMapper.java
@@ -2,41 +2,41 @@ package core;
 
 public class CapabilityMapper {
 
-    public static String AUTOMATION_NAME = "automationName";
+    public static final String AUTOMATION_NAME = "automationName";
 
-    public static String PLATFORM_NAME = "platformName";
+    public static final String PLATFORM_NAME = "platformName";
 
-    public static String PLATFORM_VERSION = "platformVersion";
+    public static final String PLATFORM_VERSION = "platformVersion";
 
-    public static String DEVICE_NAME = "deviceName";
+    public static final String DEVICE_NAME = "deviceName";
 
-    public static String APP_Path = "app";
+    public static final String APP_Path = "app";
 
-    public static String OTHER_APPS = "otherApps";
+    public static final String OTHER_APPS = "otherApps";
 
-    public static String BROWSER_NAME = "browserName";
+    public static final String BROWSER_NAME = "browserName";
 
-    public static String NEW_COMMAND_TIMEOUT = "newCommandTimeout";
+    public static final String NEW_COMMAND_TIMEOUT = "newCommandTimeout";
 
-    public static String LANGUAGE = "language";
+    public static final String LANGUAGE = "language";
 
-    public static String LOCALE = "locale";
+    public static final String LOCALE = "locale";
 
-    public static String DEVICE_UDID = "udid";
+    public static final String DEVICE_UDID = "udid";
 
-    public static String ORIENTATION = "orientation";
+    public static final String ORIENTATION = "orientation";
 
-    public static String AUTO_WEB_VIEW = "autoWebview";
+    public static final String AUTO_WEB_VIEW = "autoWebview";
 
-    public static String NO_RESET = "noReset";
+    public static final String NO_RESET = "noReset";
 
-    public static String FULL_RESET = "fullReset";
+    public static final String FULL_RESET = "fullReset";
 
-    public static String EVENT_TIMINGS = "eventTimings";
+    public static final String EVENT_TIMINGS = "eventTimings";
 
-    public static String ENABLE_PERFORMANCE_LOGGING = "enablePerformanceLogging";
+    public static final String ENABLE_PERFORMANCE_LOGGING = "enablePerformanceLogging";
 
-    public static String PRINT_PAGE_SOURCE_ON_FIND_FAILURE = "printPageSourceOnFindFailure";
+    public static final String PRINT_PAGE_SOURCE_ON_FIND_FAILURE = "printPageSourceOnFindFailure";
 
-    public static String CLEAR_SYSTEM_FILES = "clearSystemFiles";
+    public static final String CLEAR_SYSTEM_FILES = "clearSystemFiles";
 }
