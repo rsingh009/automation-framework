b6538693264eedccced1f1c948b8ef16f7acb952
Rahul Singh <rahulkumar.singh@milwaukeetool.com>
2021-12-24 06:49:36 -0600

    added capability mapper class to map the capability string with the global constant

---
+++ src/main/java/core/CapabilityMapper.java
@@ -0,0 +1,42 @@
+package core;
+
+public class CapabilityMapper {
+
+    public static String AUTOMATION_NAME = "automationName";
+
+    public static String PLATFORM_NAME = "platformName";
+
+    public static String PLATFORM_VERSION = "platformVersion";
+
+    public static String DEVICE_NAME = "deviceName";
+
+    public static String APP_Path = "app";
+
+    public static String OTHER_APPS = "otherApps";
+
+    public static String BROWSER_NAME = "browserName";
+
+    public static String NEW_COMMAND_TIMEOUT = "newCommandTimeout";
+
+    public static String LANGUAGE = "language";
+
+    public static String LOCALE = "locale";
+
+    public static String DEVICE_UDID = "udid";
+
+    public static String ORIENTATION = "orientation";
+
+    public static String AUTO_WEB_VIEW = "autoWebview";
+
+    public static String NO_RESET = "noReset";
+
+    public static String FULL_RESET = "fullReset";
+
+    public static String EVENT_TIMINGS = "eventTimings";
+
+    public static String ENABLE_PERFORMANCE_LOGGING = "enablePerformanceLogging";
+
+    public static String PRINT_PAGE_SOURCE_ON_FIND_FAILURE = "printPageSourceOnFindFailure";
+
+    public static String CLEAR_SYSTEM_FILES = "clearSystemFiles";
+}
