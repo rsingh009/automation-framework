91426b08655d0e40c6377145e5b27df633d8c144
Rahul Singh <rahulkumar.singh@milwaukeetool.com>
2021-12-29 02:43:50 -0600

    added basic user gesture methods

--- src/main/java/interfaces/Gestures.java
+++ src/main/java/interfaces/Gestures.java
@@ -1,4 +1,13 @@
 package interfaces;
 
 public interface Gestures {
+
+    void swipeLeft();
+    void swipeRight();
+    void swipeUp();
+    void swipeDown();
+    void tap();
+    void longPress();
+    void doubleTap();
+
 }
